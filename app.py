14b887a29f643f8715248c03980b2d9735513d91
GabrielSantosGue <69100286+GabrielSantosGue@users.noreply.github.com>
2020-08-02 02:40:37 -0500

    app

--- app.py
+++ app.py
@@ -18,8 +18,38 @@ app = Flask(__name__)
 #formula=0
 @app.route('/')
 def Index():
+    iris = load_iris()
+    x=iris.data
+    y=iris.target
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    a,p,r,f=RL(X_train, X_test, y_train, y_test)
+    a1,p1,r1,f1=NB(X_train, X_test, y_train, y_test)
+    a2,p2,r2,f2=RF(X_train, X_test, y_train, y_test)
+    a3,p3,r3,f3=KNN(X_train, X_test, y_train, y_test)
+    a4,p4,r4,f4=SVM(X_train, X_test, y_train, y_test)
+    a5,p5,r5,f5=n(X_train, X_test, y_train, y_test)
+    dt=[a,a1,a2,a3,a4,a5]
+    dp=[p,p1,p2,p3,p4,p5]
+    dr=[r,r1,r2,r3,r4,r5]
+    dfm=[f,f1,f2,f3,f4,f5]
+    ruta='glasss.csv'
+    dataset = pd.read_csv(ruta,delimiter=";",encoding="utf-8")
+    x=dataset.get(['RI','Na','Mg','Al','Si','K','Ca','Ba','Fe'])
+    y=dataset.get('Tipo de vidrio')
+    seed = 7
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    a,p,r,f=RL(X_train, X_test, y_train, y_test)
+    a1,p1,r1,f1=NB(X_train, X_test, y_train, y_test)
+    a2,p2,r2,f2=RF(X_train, X_test, y_train, y_test)
+    a3,p3,r3,f3=KNN(X_train, X_test, y_train, y_test)
+    a4,p4,r4,f4=SVM(X_train, X_test, y_train, y_test)
+    a5,p5,r5,f5=n(X_train, X_test, y_train, y_test)
+    rdt=[a,a1,a2,a3,a4,a5]
+    rdp=[p,p1,p2,p3,p4,p5]
+    rdr=[r,r1,r2,r3,r4,r5]
+    rdfm=[f,f1,f2,f3,f4,f5]
 
-    return render_template('index.html')
+    return render_template('index.html',dt=dt,dp=dp,dr=dr,dfm=dfm,rdt=rdt,rdp=rdp,rdr=rdr,rdfm=rdfm)
 
 @app.route('/add_resultadoRs')
 def rstudio():
@@ -30,7 +60,7 @@ def rstudio():
             lenguaje=request.form['lenguaje']
             frsul=[]
 
-            
+            frsul=textblob(buscar,numero,lenguaje)
             print("aquii!")
             dt=API(buscar,lenguaje,numero)
 
@@ -66,7 +96,7 @@ def vidrio():
             lenguaje=request.form['lenguaje']
             frsul=[]
 
-            
+            frsul=textblob(buscar,numero,lenguaje)
             print("aquii!")
             dt=API(buscar,lenguaje,numero)
 
@@ -74,26 +104,16 @@ def vidrio():
             
             ruta='glasss.csv'
             dataset = pd.read_csv(ruta,delimiter=";",encoding="utf-8")
-            #print (dataset)
             x=dataset.get(['RI','Na','Mg','Al','Si','K','Ca','Ba','Fe'])
             y=dataset.get('Tipo de vidrio')
             seed = 7
             X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
             dt=RL(X_train, X_test, y_train, y_test)
             dnb=NB(X_train, X_test, y_train, y_test)
             drf=RF(X_train, X_test, y_train, y_test)
             dknn=KNN(X_train, X_test, y_train, y_test)
             dsvm=SVM(X_train, X_test, y_train, y_test)
             dn=n(X_train, X_test, y_train, y_test)
-            # buscar="coronavirus"
-            # lenguaje="es"
-            # numero=int("10")
-            # frsul=[]
-            print("aquii!2")
-            #render_template('index.html',dt=dt,frsul=frsul)
-            # dt=API(buscar,lenguaje,numero)
-            name={"ACCURACY":"0","PRESICION":"3","RECALL":"4","F-MEASURE":"7"}
         return render_template('vidrio.html',dt=dt,dnb=dnb,drf=drf,dknn=dknn,dsvm=dsvm,dn=dn)
 
 def API(palabra,lenguaje,numero):
@@ -108,17 +128,11 @@ def RL(X_train, X_test, y_train, y_test ):
     log = LogisticRegression(multi_class='auto',solver='lbfgs')
     log.fit(X_train, y_train)
     y_pred4 = log.predict(X_test)
-    RLA="ACCURACY    :"+str(metrics.accuracy_score(y_test, y_pred4))
-    RLP="PRECISION   :"+str(metrics.precision_score(y_test, y_pred4, average='macro' ,zero_division=1))
-    RLR="RECALL    :"+str(metrics.recall_score(y_test, y_pred4, average='macro' ,zero_division=1))
-    RLF="F-MEASURE    :"+str(metrics.f1_score(y_test, y_pred4, average='macro', zero_division=1))
-    print("----------------REGRESION LOGISTICA-----------------")
-    print("Precision 1 Logística: {}".format(log.score(X_train, y_train)))
-    print("Accuracy Logistica:",metrics.accuracy_score(y_test, y_pred4))
-    print("Recall Logistica:",metrics.recall_score(y_test, y_pred4, average='macro' ,zero_division=1))
-    print("Precision 2 Logistica:",metrics.precision_score(y_test, y_pred4, average='macro' ,zero_division=1))
-    print("F1-MEASURE Logistica:",metrics.f1_score(y_test, y_pred4, average='macro', zero_division=1))
-    print("")
+    RLA=str(metrics.accuracy_score(y_test, y_pred4))
+    RLP=str(metrics.precision_score(y_test, y_pred4, average='macro' ,zero_division=1))
+    RLR=str(metrics.recall_score(y_test, y_pred4, average='macro' ,zero_division=1))
+    RLF=str(metrics.f1_score(y_test, y_pred4, average='macro', zero_division=1))
+
     return RLA,RLP,RLR,RLF
 
 
@@ -129,32 +143,22 @@ def NB(X_train, X_test, y_train, y_test ):
     nb=GaussianNB()
     nb.fit(X_train,y_train)
     y_pred3=nb.predict(X_test)
-    RLA="ACCURACY    :"+str(metrics.accuracy_score(y_test, y_pred3))
-    RLP="PRECISION    :"+str(metrics.precision_score(y_test, y_pred3, average='macro' ,zero_division=1))
-    RLR="RECALL    :"+str(metrics.recall_score(y_test, y_pred3, average='macro' ,zero_division=1))
-    RLF="F-MEASURE    :"+str(metrics.f1_score(y_test, y_pred3, average='macro', zero_division=1))
-    print("--------------------NAIVE BAYES----------------------")
-    print("Accuracy GaussianNB:",metrics.accuracy_score(y_test, y_pred3))
-    print("Recall Naive Bayes:",metrics.recall_score(y_test, y_pred3, average='macro' ,zero_division=1))
-    print("Precision Naive Bayes:",metrics.precision_score(y_test, y_pred3, average='macro' ,zero_division=1))
-    print("F1-MEASURE Naive Bayes:",metrics.f1_score(y_test, y_pred3, average='macro', zero_division=1))
-    print("")
+    RLA=str(metrics.accuracy_score(y_test, y_pred3))
+    RLP=str(metrics.precision_score(y_test, y_pred3, average='macro' ,zero_division=1))
+    RLR=str(metrics.recall_score(y_test, y_pred3, average='macro' ,zero_division=1))
+    RLF=str(metrics.f1_score(y_test, y_pred3, average='macro', zero_division=1))
+
     return RLA,RLP,RLR,RLF
 
 def RF(X_train, X_test, y_train, y_test ):
     rf=RandomForestClassifier()
     rf.fit(X_train,y_train)
     y_pred2=rf.predict(X_test)
-    RLA="ACCURACY    :"+str(metrics.accuracy_score(y_test, y_pred2))
-    RLP="PRECISION   :"+str(metrics.precision_score(y_test, y_pred2, average='macro' ,zero_division=1))
-    RLR="RECALL    :"+str(metrics.recall_score(y_test, y_pred2, average='macro' ,zero_division=1))
-    RLF="F-MEASURE    :"+str(metrics.f1_score(y_test, y_pred2, average='macro', zero_division=1))
-    print("-------------------RANDOM FOREST---------------------")
-    print("Accuracy RandomForestClassifier:",metrics.accuracy_score(y_test, y_pred2))
-    print("Recall RandomForest:",metrics.recall_score(y_test, y_pred2, average='macro' ,zero_division=1))
-    print("Precision RandomForest:",metrics.precision_score(y_test, y_pred2, average='macro' ,zero_division=1))
-    print("F1-MEASURE RandomForest:",metrics.f1_score(y_test, y_pred2, average='macro', zero_division=1))
-    print("")
+    RLA=str(metrics.accuracy_score(y_test, y_pred2))
+    RLP=str(metrics.precision_score(y_test, y_pred2, average='macro' ,zero_division=1))
+    RLR=str(metrics.recall_score(y_test, y_pred2, average='macro' ,zero_division=1))
+    RLF=str(metrics.f1_score(y_test, y_pred2, average='macro', zero_division=1))
+
 
     return RLA,RLP,RLR,RLF
 
@@ -164,18 +168,10 @@ def KNN(X_train, X_test, y_train, y_test ):
     knn = KNeighborsClassifier(n_neighbors =5, metric='minkowski', p=2)
     knn.fit(X_train,y_train)
     y_pred1=knn.predict(X_test)
-    RLA="ACCURACY    :"+str(metrics.accuracy_score(y_test, y_pred1))
-    RLP="PRECISION   :"+str(metrics.precision_score(y_test, y_pred1, average='macro' ,zero_division=1))
-    RLR="RECALL    :"+str(metrics.recall_score(y_test, y_pred1, average='macro' ,zero_division=1))
-    RLF="F-MEASURE    :"+str(metrics.f1_score(y_test, y_pred1, average='macro', zero_division=1))
-    print("------------------------KNN--------------------------")
-    print("Accuracy KNN:",metrics.accuracy_score(y_test, y_pred1))
-    print("Recall KNN:",metrics.recall_score(y_test, y_pred1, average='macro' ,zero_division=1))
-    print("Precision KNN:",metrics.precision_score(y_test, y_pred1, average='macro', zero_division=1))
-    print("F1-MEASURE KNN:",metrics.f1_score(y_test, y_pred1, average='macro', zero_division=1))
-
-    print("REPORTE:",metrics.classification_report(y_test, y_pred1))
-    print("")
+    RLA=str(metrics.accuracy_score(y_test, y_pred1))
+    RLP=str(metrics.precision_score(y_test, y_pred1, average='macro' ,zero_division=1))
+    RLR=str(metrics.recall_score(y_test, y_pred1, average='macro' ,zero_division=1))
+    RLF=str(metrics.f1_score(y_test, y_pred1, average='macro', zero_division=1))
 
     return RLA,RLP,RLR,RLF
 
@@ -184,17 +180,11 @@ def SVM(X_train, X_test, y_train, y_test ):
     msv = SVC()
     msv.fit(X_train, y_train)
     y_pred5 = msv.predict(X_test)
-    RLA="ACCURACY    :"+str(metrics.accuracy_score(y_test, y_pred5))
-    RLP="PRECISION   :"+str(metrics.precision_score(y_test, y_pred5, average='macro' ,zero_division=1))
-    RLR="RECALL    :"+str(metrics.recall_score(y_test, y_pred5, average='macro' ,zero_division=1))
-    RLF="F-MEASURE    :"+str(metrics.f1_score(y_test, y_pred5, average='macro', zero_division=1))
-    print("-------------MAQUINA DE SOPORTE VECTORIAL------------")
-    print('Precisión 1 MSV: {}'.format(msv.score(X_train, y_train)))
-    print("Accuracy MSV:",metrics.accuracy_score(y_test, y_pred5))
-    print("Recall MSV:",metrics.recall_score(y_test, y_pred5, average='macro' ,zero_division=1))
-    print("Precision 2 MSV:",metrics.precision_score(y_test, y_pred5, average='macro' ,zero_division=1))
-    print("F1-MEASURE MSV:",metrics.f1_score(y_test, y_pred5, average='macro', zero_division=1))
-    print("")
+    RLA=str(metrics.accuracy_score(y_test, y_pred5))
+    RLP=str(metrics.precision_score(y_test, y_pred5, average='macro' ,zero_division=1))
+    RLR=str(metrics.recall_score(y_test, y_pred5, average='macro' ,zero_division=1))
+    RLF=str(metrics.f1_score(y_test, y_pred5, average='macro', zero_division=1))
+
 
     return RLA,RLP,RLR,RLF
 def n(X_train, X_test, y_train, y_test):
@@ -205,18 +195,18 @@ def n(X_train, X_test, y_train, y_test):
     mlp.fit(X_train, y_train)
     Y_pred =mlp.predict(X_test)
     #print(classification_report(y_test,Y_pred))
-    RLA="ACCURACY    :"+str(metrics.accuracy_score(y_test, Y_pred))
-    RLP="PRECISION   :"+str(metrics.precision_score(y_test, Y_pred, average='macro' ,zero_division=1))
-    RLR="RECALL    :"+str(metrics.recall_score(y_test, Y_pred, average='macro' ,zero_division=1))
-    RLF="F-MEASURE    :"+str(metrics.f1_score(y_test, Y_pred, average='macro', zero_division=1))
-    print("Accuracy RED NEURONAL:",metrics.accuracy_score(y_test, Y_pred))
-    print("Recall RED NEURONAL:",metrics.recall_score(y_test, Y_pred, average='macro' ,zero_division=1))
-    print("Precision RED NEURONAL:",metrics.precision_score(y_test, Y_pred, average='macro' ,zero_division=1))
-    print("F1-MEASURE RED NEURONAL:",metrics.f1_score(y_test, Y_pred, average='macro', zero_division=1))
-    print("")
+    RLA=str(metrics.accuracy_score(y_test, Y_pred))
+    RLP=str(metrics.precision_score(y_test, Y_pred, average='macro' ,zero_division=1))
+    RLR=str(metrics.recall_score(y_test, Y_pred, average='macro' ,zero_division=1))
+    RLF=str(metrics.f1_score(y_test, Y_pred, average='macro', zero_division=1))
     return RLA,RLP,RLR,RLF
 if __name__=='__main__':
  app.run(port=3000,debug=True)
+
+
+
+
+  
 
 
 
